068cd89258d07d2a3efc322c7044f8306da9157d
DJWaddsIW <daniel.waddington@infinityworks.com>
2020-06-07 18:21:19 +0100

    func

--- utils/formatHabitatAnimal.js
+++ utils/formatHabitatAnimal.js
@@ -1,7 +1,7 @@
-module.exports = (habitatAnimal, statusKey, continentsKey, biomesKey) => {
-	const status = statusKey[habitatAnimal.status];
-	const continent = continentsKey[habitatAnimal.continent];
-	const biomes = habitatAnimal.biomes.map(habitatAnimalBiome => biomesKey[habitatAnimalBiome]);
+module.exports = (habitatAnimal, statusKeys, continentsKeys, biomesKeys) => {
+	const status = statusKeys[habitatAnimal.status];
+	const continent = continentsKeys[habitatAnimal.continent];
+	const biomes = habitatAnimal.biomes.map(habitatAnimalBiome => biomesKeys[habitatAnimalBiome]);
 	return { 
 		name: habitatAnimal.name, 
 		status, 
