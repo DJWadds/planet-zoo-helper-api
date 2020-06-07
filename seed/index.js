1b478f09abfb5828786779a45304be5bdc2f5f41
DJWaddsIW <daniel.waddington@infinityworks.com>
2020-06-07 18:15:38 +0100

    status added

--- seed/index.js
+++ seed/index.js
@@ -6,11 +6,23 @@ const UserRoles = require("../models/mUserRoles");
 const EnrichmentItems = require("../models/mEnrichmentItems");
 const Continents = require("../models/mContinents");
 const Biomes = require("../models/mBiomes");
+const Status = require("../models/mStatus");
 
 const formatUser = require("../utils/formatUser");
 const createUserRolesKey = require("../utils/createUserRolesKey");
 
-function seedDB(usersData, barriers, enrichmentItems, continents, biomes) {
+const status = [
+	{ name: "Unkown", level: 0 },
+	{ name: "Domesticated", level: 1 },
+	{ name: "Least Concern", level: 2 },
+	{ name: "Near Threatened", level: 3 },
+	{ name: "Vulnerable", level: 4 },
+	{ name: "Endangered", level: 5 },
+	{ name: "Critically Endangered", level: 6 },
+	{ name: "Extinct in Wild", level: 7 }
+]
+
+function seedDB(usersData, barriers, enrichmentItems, continents, biomes, habitatAnimals) {
 	return mongoose.connection
 		.dropDatabase()
 		.then(() => {
@@ -22,26 +34,30 @@ function seedDB(usersData, barriers, enrichmentItems, continents, biomes) {
 				EnrichmentItems.insertMany(enrichmentItems),
 				Continents.insertMany(continents),
 				Biomes.insertMany(biomes),
+				Status.insertMany(status)
 			]);
 		})
-		.then(([userRoleDocs, barrierDocs, enrichmentItemsDocs, continentDocs, biomeDocs]) => {
+		.then(([userRoleDocs, barrierDocs, enrichmentItemsDocs, continentDocs, biomeDocs, statusDocs]) => {
 			console.log(`inserted ${userRoleDocs.length} user roles`);
 			console.log(`inserted ${barrierDocs.length} barriers`);
 			console.log(`inserted ${enrichmentItemsDocs.length} enrichment items`);
 			console.log(`inserted ${continentDocs.length} continents`);
 			console.log(`inserted ${biomeDocs.length} biomes`);
+			console.log(`inserted ${statusDocs.length} status`);
 			const userRoleKey = createUserRolesKey(userRoleDocs);
 			const formattedUsers = usersData.map(user => formatUser(user, userRoleKey));
+			// const 
 			return Promise.all([
 				userRoleDocs,
 				barrierDocs,
 				enrichmentItemsDocs,
 				continentDocs, 
 				biomeDocs,
+				statusDocs,
 				Users.insertMany(formattedUsers)
 			]);
 		})
-		.then(([userRoleDocs, barrierDocs, enrichmentItemsDocs, continentDocs, biomeDocs, userDocs]) => {
+		.then(([userRoleDocs, barrierDocs, enrichmentItemsDocs, continentDocs, biomeDocs, statusDocs, userDocs]) => {
 			console.log(`inserted ${userDocs.length} users`);
 			return { userDocs, userRoleDocs, barrierDocs, enrichmentItemsDocs, continentDocs, biomeDocs };
 		});
